6a46053f2665f95625a43405bb21fdbd7db50dee
Admin <admin@Admins-MacBook-Pro.local>
2025-03-31 12:34:08 +0200

    Перше завантаження TradePilot

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
+import os
 import json
 
 app = Flask(__name__)
 
+# Завантаження списку активів з файлу
+with open("assets.json", "r") as file:
+    ASSETS = json.load(file)
+
 @app.route('/')
 def index():
-    with open('assets.json', 'r') as file:
-        assets = json.load(file)['forex']
-    return render_template('index.html', assets=assets)
+    selected_asset = request.args.get('asset', 'EURUSD')
+    return render_template('index.html', selected_asset=selected_asset, assets=ASSETS)
 
+# Запуск з правильним портом
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
